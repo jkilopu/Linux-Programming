2114bf760b4e25e770cb5942ea260962eae0a85c
Isaac <a18786717618@126.com>
2020-04-07 09:11:32 +0800

    popen：将进程当作文件

--- unix和linux编程实践教程-源代码/ch11/bookcode/popen.c
+++ unix和linux编程实践教程-源代码/ch11/bookcode/popen.c
@@ -6,41 +6,47 @@
  *		execls "sh" "-c" command
  *    todo: what about signal handling for child process?
  */
-#include	<stdio.h>
-#include	<signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
-#define	READ	0
-#define	WRITE	1
+#define READ 0
+#define WRITE 1
 
 main()
 {
-	FILE	*fp;
-	char	buf[BUFSIZ];
+	FILE *fp;
+	char buf[BUFSIZ];
 
-	fp = popen("ls","r");
-	while( fgets(buf, BUFSIZ,fp) != NULL)
+	fp = popen("ls", "r");
+	while (fgets(buf, BUFSIZ, fp) != NULL)
 		fputs(buf, stdout);
 }
 
-
 FILE *popen(const char *command, const char *mode)
 {
-	int	pfp[2], pid;		/* the pipe and the process	*/
-	FILE	*fdopen(), *fp;		/* fdopen makes a fd a stream	*/
-	int	parent_end, child_end;	/* of pipe 			*/
+	int pfp[2], pid;		   /* the pipe and the process	*/
+	FILE *fdopen(), *fp;	   /* fdopen makes a fd a stream	*/
+	int parent_end, child_end; /* of pipe 			*/
 
-	if ( *mode == 'r' ){		/* figure out direction		*/
+	if (*mode == 'r')
+	{ /* figure out direction		*/
 		parent_end = READ;
-		child_end = WRITE ;
-	} else if ( *mode == 'w' ){
+		child_end = WRITE;
+	}
+	else if (*mode == 'w')
+	{
 		parent_end = WRITE;
-	       	child_end = READ ;
-	} else return NULL ;
-
-	if ( pipe(pfp) == -1 ) 			/* get a pipe		*/
+		child_end = READ;
+	}
+	else
 		return NULL;
-	if ( (pid = fork()) == -1 ){		/* and a process	*/
-		close(pfp[0]);			/* or dispose of pipe	*/
+
+	if (pipe(pfp) == -1) /* get a pipe		*/
+		return NULL;
+	if ((pid = fork()) == -1)
+	{				   /* and a process	*/
+		close(pfp[0]); /* or dispose of pipe	*/
 		close(pfp[1]);
 		return NULL;
 	}
@@ -48,24 +54,25 @@ FILE *popen(const char *command, const char *mode)
 	/* --------------- parent code here ------------------- */
 	/*   need to close one end and fdopen other end		*/
 
-	if ( pid > 0 ){	
-		if (close( pfp[child_end] ) == -1 )
+	if (pid > 0)
+	{
+		if (close(pfp[child_end]) == -1)
 			return NULL;
-		return fdopen( pfp[parent_end] , mode);	/* same mode */
+		return fdopen(pfp[parent_end], mode); /* same mode */
 	}
 
 	/* --------------- child code here --------------------- */
 	/*   need to redirect stdin or stdout then exec the cmd	 */
 
-	if ( close(pfp[parent_end]) == -1 )	/* close the other end	*/
-		exit(1);			/* do NOT return	*/
+	if (close(pfp[parent_end]) == -1) /* close the other end	*/
+		exit(1);					  /* do NOT return	*/
 
-	if ( dup2(pfp[child_end], child_end) == -1 )
+	if (dup2(pfp[child_end], child_end) == -1)
 		exit(1);
 
-	if ( close(pfp[child_end]) == -1 )	/* done with this one	*/
+	if (close(pfp[child_end]) == -1) /* done with this one	*/
 		exit(1);
-						/* all set to run cmd	*/
-	execl( "/bin/sh", "sh", "-c", command, NULL );
+	/* all set to run cmd	*/
+	execl("/bin/sh", "sh", "-c", command, NULL);
 	exit(1);
 }
